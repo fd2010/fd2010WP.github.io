56a730bbb2cb79bbc28f2b01ce985b948839d2d8
fd2010 <144828440+fd2010@users.noreply.github.com>
2023-09-24 00:13:50 +0100

    sign-in-able website

--- javatest.js
+++ javatest.js
@@ -4,8 +4,14 @@ function logOn() {
 
   // Check if the user provided a name
   if (userName !== null && userName !== "") {
-      var userName = prompt("Hello, " + userName + "!  please enter password next");
+      var password = prompt("Hello, " + userName + "!  please enter password next");
       alert("Sign in successfull");
+      document.getElementById("logOnButton").style.display = "none";
+
+      //update logout text
+      var outputParagraph = document.getElementById("displayUser");
+      outputParagraph.textContent = "Logged in as: " + userName;
+
   } else {
       alert("*Log in unsuccesfull*");
   }
