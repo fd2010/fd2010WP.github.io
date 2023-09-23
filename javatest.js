74b0146870b5bf46b563499f6ade01b2cbee0918
fd2010 <144828440+fd2010@users.noreply.github.com>
2023-09-23 23:33:13 +0100

    .

--- javatest.js
+++ javatest.js
@@ -4,11 +4,9 @@ function logOn() {
 
   // Check if the user provided a name
   if (userName !== null && userName !== "") {
-      // Display a greeting with the user's name
       var userName = prompt("Hello, " + userName + "!  please enter password next");
       alert("Sign in successfull");
   } else {
-      // If the user didn't provide a name, display a generic greeting
       alert("*Log in unsuccesfull*");
   }
 }
