9ee4d42ef0f659f14d17aaa7eb2ca26edd843a9f
fd2010 <144828440+fd2010@users.noreply.github.com>
2023-11-02 11:13:07 +0000

    AJAX object movement

--- AjaxXmlRss.js
+++ AjaxXmlRss.js
@@ -14,3 +14,22 @@ function getContents() {
         }
     });
 }
+
+
+// Move the white box to a random position
+function moveBoxRandomly() {
+    var whiteBox = document.getElementById('whiteBox');
+    var maxX = window.innerWidth - whiteBox.clientWidth;
+    var maxY = window.innerHeight - whiteBox.clientHeight;
+  
+    var randomX = Math.floor(Math.random() * maxX);
+    var randomY = Math.floor(Math.random() * maxY);
+  
+    whiteBox.style.left = randomX + 'px';
+    whiteBox.style.top = randomY + 'px';
+  }
+  
+  // Attach the function to a button click event
+  var moveButton = document.getElementById('moveButton');
+  moveButton.addEventListener('click', moveBoxRandomly);
+  
