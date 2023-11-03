8fc7ab9244d6054c68a7d3240aecc8be51dc9ad1
fd2010 <144828440+fd2010@users.noreply.github.com>
2023-11-03 20:25:32 +0000

    example of hangman game implemented

--- JSon.js
+++ JSon.js
@@ -20,3 +20,68 @@ function runJSONProgram(){
 }
 
 document.getElementById("runJsonButton").addEventListener("click", runJSONProgram);
+
+
+
+// List of words to choose from
+const words = ["javascript", "hangman", "developer", "programming", "openai"];
+
+// Choose a random word from the list
+const wordToGuess = words[Math.floor(Math.random() * words.length)];
+
+// Initialize variables
+let guessedWord = Array(wordToGuess.length).fill("_");
+let incorrectGuesses = [];
+let guessesLeft = 6;
+
+// DOM elements
+const wordDisplay = document.getElementById("word-display");
+const guessInput = document.getElementById("guess-input");
+const guessButton = document.getElementById("guess-button");
+const guessesLeftDisplay = document.getElementById("guesses-left");
+const incorrectGuessesDisplay = document.getElementById("incorrect-guesses");
+
+// Display initial state
+wordDisplay.textContent = guessedWord.join(" ");
+guessesLeftDisplay.textContent = guessesLeft;
+
+// Event listener for the guess button
+guessButton.addEventListener("click", function () {
+  const guess = guessInput.value.toLowerCase();
+  guessInput.value = "";
+
+  if (guess.length !== 1 || !guess.match(/[a-z]/)) {
+    alert("Please enter a single letter.");
+    return;
+  }
+
+  if (guessedWord.includes(guess) || incorrectGuesses.includes(guess)) {
+    alert("You've already guessed this letter.");
+    return;
+  }
+
+  if (wordToGuess.includes(guess)) {
+    for (let i = 0; i < wordToGuess.length; i++) {
+      if (wordToGuess[i] === guess) {
+        guessedWord[i] = guess;
+      }
+    }
+
+    wordDisplay.textContent = guessedWord.join(" ");
+
+    if (!guessedWord.includes("_")) {
+      alert("Congratulations! You guessed the word: " + wordToGuess);
+      location.reload();
+    }
+  } else {
+    incorrectGuesses.push(guess);
+    incorrectGuessesDisplay.textContent = incorrectGuesses.join(", ");
+    guessesLeft--;
+    guessesLeftDisplay.textContent = guessesLeft;
+
+    if (guessesLeft === 0) {
+      alert("Game over! The word was: " + wordToGuess);
+      location.reload();
+    }
+  }
+});
