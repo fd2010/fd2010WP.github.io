f8cc3dfa6e37bfd1f3ca780df6874811827f1645
fd2010 <144828440+fd2010@users.noreply.github.com>
2023-11-03 11:47:45 +0000

    Added a bit of nodeJS

--- node.js
+++ node.js
@@ -1,40 +1,54 @@
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var http = require('http');
-var app = express();
-app.use(cookieParser());
-var head = ["<!DOCTYPE html>", "<html>", "<head>",
-             "<title>Cookies</title>", "</head>", "<body>"];
-var url = "http://localhost:8082/cookie";
-var tail = [`<form action=${url}>`,
-            '<p>', 'Key <input name="name" value="">', '</p>',
-            '<p>', 'Value <input name="value" value="">', '</p>',
-            '<p>', 'Age <input name="age" value="">', '</p>',
-            '<p>', '<input type="submit"/>', 
-            '</form>', '</body>', '</html>'];
-var eol = '\n';
-app.get('/', function (req, res) {
-    res.send("Welcome to Cookie Test app!");
-});
-app.get('/cookie', function (req, res) {
-    var s = '';
-    for (var i in head) s += head[i] + eol;
-    if ( req.query.name != undefined && req.query.name != '' &&
-            req.query.value != undefined ) {
-        if ( isNaN(req.query.age) ) 
-            req.query.age = 0;
-        res.cookie(req.query.name, req.query.value, {maxAge: req.query.age});        
-    }
-    s += '<p>' + eol;
-    for (var i in req.cookies)
-        s += i + '=' + req.cookies[i] + '<br>' + eol;
-    s += '</p>' + eol;
+const button = document.getElementById('nodeJSExample');
+const message = document.getElementById('message');
 
-    for (var i in tail) s += tail[i] + eol;
-    res.send(s); // send web page
+button.addEventListener('click', () => {
+    message.textContent = 'Button clicked! This is a Node.js example.';
 });
 
-app.listen(8082, function() {console.log("Cookie test started on port 8082");});
+
+
+
+
+
+
+
+// var express = require('express');
+// var cookieParser = require('cookie-parser');
+// var http = require('http');
+// var app = express();
+// app.use(cookieParser());
+// var head = ["<!DOCTYPE html>", "<html>", "<head>",
+//              "<title>Cookies</title>", "</head>", "<body>"];
+// var url = "http://localhost:8082/cookie";
+// var tail = [`<form action=${url}>`,
+//             '<p>', 'Key <input name="name" value="">', '</p>',
+//             '<p>', 'Value <input name="value" value="">', '</p>',
+//             '<p>', 'Age <input name="age" value="">', '</p>',
+//             '<p>', '<input type="submit"/>', 
+//             '</form>', '</body>', '</html>'];
+// var eol = '\n';
+// app.get('/', function (req, res) {
+//     res.send("Welcome to Cookie Test app!");
+// });
+// app.get('/cookie', function (req, res) {
+//     var s = '';
+//     for (var i in head) s += head[i] + eol;
+//     if ( req.query.name != undefined && req.query.name != '' &&
+//             req.query.value != undefined ) {
+//         if ( isNaN(req.query.age) ) 
+//             req.query.age = 0;
+//         res.cookie(req.query.name, req.query.value, {maxAge: req.query.age});        
+//     }
+//     s += '<p>' + eol;
+//     for (var i in req.cookies)
+//         s += i + '=' + req.cookies[i] + '<br>' + eol;
+//     s += '</p>' + eol;
+
+//     for (var i in tail) s += tail[i] + eol;
+//     res.send(s); // send web page
+// });
+
+// app.listen(8082, function() {console.log("Cookie test started on port 8082");});
 
 
 
